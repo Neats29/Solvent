da5ec79a57d8374567cfc73d42c7e4929fa6c30f
Anita Amini <baadge29@yahoo.co.uk>
2015-12-08 18:04:28 +0000

    Update odd.js

--- odd.js
+++ odd.js
@@ -1,5 +1,8 @@
 //From an array of integers, return the integer that occurs odd number of times
 
-const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+const odd = (xs) => xs.reduce((a, b) => a ^ b);
+
+//odd([1, 1, 2, 2, 3, 3, 4, 5, 5])
+// returns 4
 
 //This uses the XOR opperator, see more here: https://en.wikipedia.org/wiki/Exclusive_or
