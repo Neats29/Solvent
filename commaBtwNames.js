5f16d25e943e8e95a6f12f4f2f24f8400078ae3e
Anita Amini <baadge29@yahoo.co.uk>
2015-12-09 17:49:15 +0000

    another answer

---
+++ commaBtwNames.js
@@ -0,0 +1,9 @@
+//Given a list of names, return names in the following format: 
+//list([ {name: 'Sam}, {name: 'Matt'}, {name: 'Fred'} ])
+// should return 'Sam, Matt & Fred'
+
+function list(names){
+  return names.reduce(function(prev, curr, i, arr){
+    return prev + curr.name + (i < arr.length-2 ? ', ' : i == arr.length - 2 ? ' & ' : '');
+  }, '');
+}
