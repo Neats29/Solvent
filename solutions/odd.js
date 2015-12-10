a522454779995ed8605e2903252e7a0ceca91754
Anita Amini <baadge29@yahoo.co.uk>
2015-12-10 15:33:13 +0000

    Update odd.js

--- solutions/odd.js
+++ solutions/odd.js
@@ -6,3 +6,28 @@ const odd = (xs) => xs.reduce((a, b) => a ^ b);
 // returns 4
 
 //This uses the XOR opperator, see more here: https://en.wikipedia.org/wiki/Exclusive_or
+
+
+
+//Normal (expanded answer for alternative tasks)
+  var arr = []
+  var copy = A.slice(0);
+  for (var i = 0; i < A.length; i++) {
+     var count = 0;
+     for (var j = 0; j < copy.length; j++) {
+       if (A[i] == copy[j]) {
+          count++;
+          delete copy[j];
+       }
+     }
+        if (count > 0) {
+   			var obj = {};
+   			obj.value = A[i];
+   			obj.Count = count;
+   			arr.push(obj);
+ 		    }
+   }
+   var x;
+   arr.filter((a, i) => a.Count % 2 !== 0 ? x = a.value : false );
+   return x
+ }
