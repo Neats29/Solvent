d4ff9c68bfb087e8874fdd1b357a77a939765ef4
Anita Amini <baadge29@yahoo.co.uk>
2015-12-17 18:31:14 +0000

    Create intArray.js

---
+++ solutions/intArray.js
@@ -0,0 +1,10 @@
+//If arguments is an array and all elements are integers, return true
+
+function isIntArray(arr) {
+  return Array.isArray(arr) && arr.every(function (x) { return Math.floor(x) === x });
+}
+
+isIntArray([1, 2, 3, NaN]) //false
+isIntArray([5.56789e-0, 2]) //false
+isIntArray([10, 20, 236, 324) //true
+isIntArray([]) //true
