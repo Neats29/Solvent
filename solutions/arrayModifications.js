879b01060a0b640c903fe95d02ef40f9c7e6ce1f
Anita Amini <baadge29@yahoo.co.uk>
2016-01-01 14:13:52 +0000

    Update arrayModifications.js

--- solutions/arrayModifications.js
+++ solutions/arrayModifications.js
@@ -1,6 +1,6 @@
 //Make changes to arrays without mutating them:
 
-//1. Add a new element to the array:
+////////////1. Add a new element to the array:
 
 var arr = [1, 2, 3];
 //Mutable:
@@ -8,6 +8,6 @@ arr.push(4)
 console.log(arr) // arr = [1,2,3,4] 
 
 //Immutable solution:
-console.log(  arr.concat([4])   ); // [1,2,3,4]
+console.log(        arr.concat([4])         ); // [1,2,3,4]
 //using the ES6 Spread operator:
-console.log(  [...arr,4]   ); // [1,2,3,4]
+console.log(         [...arr,4]             ); // [1,2,3,4]
