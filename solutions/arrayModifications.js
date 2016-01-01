df3f36b3dd9de6304ad4de79f574c1ec45c0334c
Anita Amini <baadge29@yahoo.co.uk>
2016-01-01 14:19:52 +0000

    Update arrayModifications.js

--- solutions/arrayModifications.js
+++ solutions/arrayModifications.js
@@ -1,8 +1,9 @@
 //Make changes to arrays without mutating them:
 
-////////////1. Add a new element to the array:
-
 var arr = [1, 2, 3];
+var index = 1;
+//*********1. Add a new element to the array: ************
+
 //Mutable:
 arr.push(4) 
 console.log(arr) // arr = [1,2,3,4] 
@@ -11,3 +12,14 @@ console.log(arr) // arr = [1,2,3,4]
 console.log(        arr.concat([4])         ); // [1,2,3,4]
 //using the ES6 Spread operator:
 console.log(         [...arr,4]             ); // [1,2,3,4]
+
+//*********2. Remove an element in the array: ************
+
+//Mutable:
+arr.splice(1,1);
+console.log(arr) // arr = [1,3]
+
+//Immutable solution:
+console.log(        arr.slice(0,index).concat(arr.slice(index+1))         ); // [1,3]
+//using the ES6 Spread operator:
+console.log(       [...arr.slice(0,index), ...list.slice(index+1)]         ); // [1,3]
